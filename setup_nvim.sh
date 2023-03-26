725f7d85bf2011ced354e9d1f88a0908108b0e7f
Piyush Pradhan <ppradhan@xyz.com>
2023-03-26 15:42:15 -0400

    nvim config

--- setup_nvim.sh
+++ setup_nvim.sh
@@ -2,6 +2,5 @@
 
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
-git clone https://github.com/p0209p/Neovim_Config
 cp -r nvim ~/.config/
-echo("Download Packer.nvim using its repo. Run :PackerSync in nvim to install everything !")
+echo("Setup Complete !")
